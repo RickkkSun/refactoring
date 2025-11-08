c3e5bf38d2db5dbe804550d828414697eb335a0b
RickkkSun <yimingooo.sun@mail.utoronto.ca>
2025-11-08 18:13:02 -0500

    Task 2.1: Extract getAmount() and getPlay()

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,21 @@
 package theater;
 
+/** A play in the catalog. */
 public class Play {
-
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
+
